60e373fb5d34c9d89829e5cfc1441e8bedfd5d27
JoelGuzman-arch <josepjoel.guzman@estudiant.enti.cat>
2026-02-15 23:32:28 +0100

    añadimos editar_rotores, comprobaciones.h y comprabaciones.cpp rotor.h y rotor.cpp

--- Enigma_Joel_Martin/Enigma_Joel_Martin/comprobaciones.h
+++ Enigma_Joel_Martin/Enigma_Joel_Martin/comprobaciones.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <string>
+
+inline bool esLetraMayuscula(char c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+bool esRotorValido(const std::string& rotor);
+
--- Enigma_Joel_Martin/Enigma_Joel_Martin/editar_rotores.h
+++ Enigma_Joel_Martin/Enigma_Joel_Martin/editar_rotores.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void editar_rotor();
