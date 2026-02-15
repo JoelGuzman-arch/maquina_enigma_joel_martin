90773835a924ec4ab2788c2c49fb725e02d9c589
JoelGuzman-arch <josepjoel.guzman@estudiant.enti.cat>
2026-02-15 23:39:34 +0100

    añadi descifrar.cpp y descifrar.h

--- Enigma_Joel_Martin/Enigma_Joel_Martin/descifrar.h
+++ Enigma_Joel_Martin/Enigma_Joel_Martin/descifrar.h
@@ -1,1 +1,13 @@
 #pragma once
+
+
+/**
+ * @brief Descifra un mensaje utilizando tres rotores.
+ *
+ * La función lee los rotores desde archivos, pide un mensaje cifrado
+ * y lo procesa letra por letra para obtener el mensaje original.
+ * Para que funcione correctamente, las posiciones iniciales de los
+ * rotores deben ser las mismas que se usaron al cifrar.
+ */
+
+void descifrar(int primera_posicion_indice, int segunda_posicion_indice, int tercera_posicion_indice);
